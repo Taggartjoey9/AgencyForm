5230303fddd37bd29c5e4e1b1cdcdaa0f9509c17
Taggartjoey9 <taggartjoey9@gmail.com>
2023-01-02 00:30:50 -0500

    Logic for buttons added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Leftside from "./Leftside";
+import {useState} from 'react';
 
 function App() {
+
+const [stage, setStage] = useState(0);
+
   return (
-    <Leftside />
+    <Leftside stage={stage} setStage={setStage} />
   );
 }
 
--- my-app/src/Leftside.js
+++ my-app/src/Leftside.js
@@ -1,10 +1,41 @@
-export default function Leftside() {
+const LABEL_TEXT = [
+    'Apply to work with our agency',
+    'What\'s your budget?',
+    'Anything else we should know?',
+    'Thank you!'
+]
+
+export default function Leftside({stage, setStage}) {
+
+
+    let content;
+        
+        if (stage === 0) {
+            content = (
+                <input type="text" placeholder="What's your email?" />       
+            )
+        } else if (stage === 1) {
+            content = (
+
+                <div className='button-group'>
+                    <button>$2k</button>
+                    <button>$5k</button>
+                    <button>No Limit</button>
+                </div>
+            )
+        } else if (stage === 2) {
+            content = (
+                <input type="text" placeholder=""></input>       
+            )
+        };
+
     return (
+
         <div className="container">
             <div className="left-text">
-                <h2>Apply to work at our agency</h2>
-                <input type="text" placeholder="What's your email?"></input>
-                <button>Next</button>
+                <h1>{LABEL_TEXT[stage]}</h1>
+                {content}
+                {stage !== 3 && (<button className="next-button" onClick={() => setStage(stage + 1)}>Next</button>)}
             </div>
             <div className="right-img"></div>
         </div>
